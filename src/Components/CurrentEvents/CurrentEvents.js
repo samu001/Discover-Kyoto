ba2aaf023833f97be225b112f635006189730e65
Samuel PF <samss01one@gmail.com>
2022-10-21 23:46:40 -0400

    Filter by Month Event
    Added a dropdown filter, to select the events by month

--- src/Components/CurrentEvents/CurrentEvents.js
+++ src/Components/CurrentEvents/CurrentEvents.js
@@ -2,27 +2,66 @@ import React, { useState } from "react";
 import CardData from "../../Data/CardData";
 import EventCard from "../EventCard/EventCard";
 import "./CurrentEvents.css";
+import Select from "react-select";
 
 export default function CurrentEvents() {
     const [cardData, setCardData] = useState(CardData);
 
+    const currentMonth = parseInt(`${new Date().getMonth() + 1}`);
+    const [month, setMonth] = useState(currentMonth);
+
+    console.log(month);
+
+    const handleMonthChange = (selected) => {
+        setMonth(selected.value);
+    };
+
+    const options = [
+        { value: 1, label: "January" },
+        { value: 2, label: "February" },
+        { value: 3, label: "March" },
+        { value: 4, label: "April" },
+        { value: 5, label: "May" },
+        { value: 6, label: "June" },
+        { value: 7, label: "July" },
+        { value: 8, label: "August" },
+        { value: 9, label: "September" },
+        { value: 10, label: "October" },
+        { value: 11, label: "November" },
+        { value: 12, label: "December" },
+    ];
     // This will map all the data from the CardsData object array and display them as a EventCard Element
-    const cardElements = cardData.map((card) => {
-        return (
-            <EventCard
-                title={card.title}
-                location={card.location}
-                description={card.description}
-                img={card.img}
-                locUrl={card.locUrl}
-                key={card.title}
+
+    const filterBox = (
+        <div className="event-selector-box">
+            <Select
+                options={options}
+                onChange={handleMonthChange}
+                defaultValue={month}
+                placeholder={options[month - 1].label}
             />
-        );
+        </div>
+    );
+
+    const cardElements = cardData.map((card) => {
+        if (card.month === month) {
+            return (
+                <EventCard
+                    title={card.title}
+                    location={card.location}
+                    description={card.description}
+                    img={card.img}
+                    locUrl={card.locUrl}
+                    key={card.title}
+                />
+            );
+        }
     });
 
     return (
         <div className="events-section" id="events">
             <div className="card-container">{cardElements}</div>
+            <div>{filterBox}</div>
         </div>
     );
 }
--- src/Data/CardData.js
+++ src/Data/CardData.js
@@ -1,20 +1,21 @@
+// Events Data
 export default [
     {
-        title: "Nigth Matsuri",
+        title: "New Year Festival",
         location: "Nishiki Market",
         locUrl: "https://www.google.com/maps/place/Nishiki+Market/@35.0049808,135.7626718,17z/data=!3m1!4b1!4m5!3m4!1s0x6001089ccd8ccb4f:0xb69ea31001ec6c9c!8m2!3d35.0049808!4d135.7648605",
         description:
             "Check all the food and games in our stunning Nigth festival, perfect fit for the whole family.",
-        month: 2,
+        month: 1,
         img: "https://images.unsplash.com/photo-1494588024300-e9df7ff98d78?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8amFwYW4lMjBjdWx0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q",
     },
     {
-        title: "Guided Tour",
+        title: "New Year wine showcase",
         location: "Kyoto Station",
         locUrl: "https://www.google.com/maps/place/Kyoto+Station/@34.9849887,135.7569952,16.38z/data=!4m5!3m4!1s0x600108ae918b02ef:0xb61a446e74a21c08!8m2!3d34.985849!4d135.7587667",
         description:
             "Join a personalized tour with a knowledgeable expert in Japanese culture and history.",
-        month: 2,
+        month: 1,
         img: "https://images.unsplash.com/photo-1613590928141-93eb1bbfb59f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGt5b3RvfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
     },
     {
@@ -26,18 +27,174 @@ export default [
         month: 2,
         img: "https://images.unsplash.com/photo-1575548393466-0df1618ba410?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
     },
-    // {
-    //     title: "Sample 4",
-    //     location: "Location 4",
-    //     description: "Lorem ipsum sample description of card text 4",
-    //     month: 3,
-    //     img: "https://images.unsplash.com/photo-1531686264889-56fdcabd163f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmlyZXdvcmtzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
-    // },
-    // {
-    //     title: "Sample 5",
-    //     location: "Location 5",
-    //     month: 3,
-    //     description: "Lorem ipsum sample description of card text 5",
-    //     img: "https://images.unsplash.com/photo-1581536763020-d2d7cfdd4df6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGt5b3RvfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
-    // },
+    {
+        title: "Montain Bike Track",
+        location: "The Bamboo Forest",
+        locUrl: "https://www.google.com/maps/place/Kyoto+Bike+Tour+-+The+Bamboo+Forest+%26+Arashiyama/@35.0194942,135.670815,18z/data=!3m1!4b1!4m5!3m4!1s0x6001a99346427e6f:0xd42663eba40842ac!8m2!3d35.0194942!4d135.6719093",
+        description:
+            "Kyoto's scenic nature and mountains are perfect for an unforgettable bike tour.",
+        month: 3,
+        img: "https://images.unsplash.com/photo-1575548393466-0df1618ba410?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    },
+    {
+        title: "Tea ceremony",
+        location: "The Bamboo Forest",
+        locUrl: "https://www.google.com/maps/place/Kyoto+Bike+Tour+-+The+Bamboo+Forest+%26+Arashiyama/@35.0194942,135.670815,18z/data=!3m1!4b1!4m5!3m4!1s0x6001a99346427e6f:0xd42663eba40842ac!8m2!3d35.0194942!4d135.6719093",
+        description:
+            "Kyoto's scenic nature and mountains are perfect for an unforgettable bike tour.",
+        month: 4,
+        img: "https://images.unsplash.com/photo-1480796927426-f609979314bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+        title: "Montain Bike Track",
+        location: "The Bamboo Forest",
+        locUrl: "https://www.google.com/maps/place/Kyoto+Bike+Tour+-+The+Bamboo+Forest+%26+Arashiyama/@35.0194942,135.670815,18z/data=!3m1!4b1!4m5!3m4!1s0x6001a99346427e6f:0xd42663eba40842ac!8m2!3d35.0194942!4d135.6719093",
+        description:
+            "Kyoto's scenic nature and mountains are perfect for an unforgettable bike tour.",
+        month: 4,
+        img: "https://images.unsplash.com/photo-1575548393466-0df1618ba410?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    },
+    {
+        title: "Montain Bike Track",
+        location: "The Bamboo Forest",
+        locUrl: "https://www.google.com/maps/place/Kyoto+Bike+Tour+-+The+Bamboo+Forest+%26+Arashiyama/@35.0194942,135.670815,18z/data=!3m1!4b1!4m5!3m4!1s0x6001a99346427e6f:0xd42663eba40842ac!8m2!3d35.0194942!4d135.6719093",
+        description:
+            "Kyoto's scenic nature and mountains are perfect for an unforgettable bike tour.",
+        month: 5,
+        img: "https://images.unsplash.com/photo-1575548393466-0df1618ba410?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    },
+    {
+        title: "Montain Bike Track",
+        location: "The Bamboo Forest",
+        locUrl: "https://www.google.com/maps/place/Kyoto+Bike+Tour+-+The+Bamboo+Forest+%26+Arashiyama/@35.0194942,135.670815,18z/data=!3m1!4b1!4m5!3m4!1s0x6001a99346427e6f:0xd42663eba40842ac!8m2!3d35.0194942!4d135.6719093",
+        description:
+            "Kyoto's scenic nature and mountains are perfect for an unforgettable bike tour.",
+        month: 5,
+        img: "https://images.unsplash.com/photo-1575548393466-0df1618ba410?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    },
+    {
+        title: "Montain Bike Track",
+        location: "The Bamboo Forest",
+        locUrl: "https://www.google.com/maps/place/Kyoto+Bike+Tour+-+The+Bamboo+Forest+%26+Arashiyama/@35.0194942,135.670815,18z/data=!3m1!4b1!4m5!3m4!1s0x6001a99346427e6f:0xd42663eba40842ac!8m2!3d35.0194942!4d135.6719093",
+        description:
+            "Kyoto's scenic nature and mountains are perfect for an unforgettable bike tour.",
+        month: 6,
+        img: "https://images.unsplash.com/photo-1575548393466-0df1618ba410?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    },
+    {
+        title: "June Go Carts",
+        location: "The Bamboo Forest",
+        locUrl: "https://www.google.com/maps/place/Kyoto+Bike+Tour+-+The+Bamboo+Forest+%26+Arashiyama/@35.0194942,135.670815,18z/data=!3m1!4b1!4m5!3m4!1s0x6001a99346427e6f:0xd42663eba40842ac!8m2!3d35.0194942!4d135.6719093",
+        description:
+            "Kyoto's scenic nature and mountains are perfect for an unforgettable bike tour.",
+        month: 6,
+        img: "https://images.unsplash.com/photo-1575548393466-0df1618ba410?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    },
+    {
+        title: "Montain Bike Track",
+        location: "The Bamboo Forest",
+        locUrl: "https://www.google.com/maps/place/Kyoto+Bike+Tour+-+The+Bamboo+Forest+%26+Arashiyama/@35.0194942,135.670815,18z/data=!3m1!4b1!4m5!3m4!1s0x6001a99346427e6f:0xd42663eba40842ac!8m2!3d35.0194942!4d135.6719093",
+        description:
+            "Kyoto's scenic nature and mountains are perfect for an unforgettable bike tour.",
+        month: 7,
+        img: "https://images.unsplash.com/photo-1575548393466-0df1618ba410?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    },
+    {
+        title: "Montain Bike Track",
+        location: "The Bamboo Forest",
+        locUrl: "https://www.google.com/maps/place/Kyoto+Bike+Tour+-+The+Bamboo+Forest+%26+Arashiyama/@35.0194942,135.670815,18z/data=!3m1!4b1!4m5!3m4!1s0x6001a99346427e6f:0xd42663eba40842ac!8m2!3d35.0194942!4d135.6719093",
+        description:
+            "Kyoto's scenic nature and mountains are perfect for an unforgettable bike tour.",
+        month: 7,
+        img: "https://images.unsplash.com/photo-1575548393466-0df1618ba410?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    },
+    {
+        title: "Japan Baseball Training",
+        location: "The Bamboo Forest",
+        locUrl: "https://www.google.com/maps/place/Kyoto+Bike+Tour+-+The+Bamboo+Forest+%26+Arashiyama/@35.0194942,135.670815,18z/data=!3m1!4b1!4m5!3m4!1s0x6001a99346427e6f:0xd42663eba40842ac!8m2!3d35.0194942!4d135.6719093",
+        description:
+            "Kyoto's scenic nature and mountains are perfect for an unforgettable bike tour.",
+        month: 8,
+        img: "https://images.unsplash.com/photo-1575548393466-0df1618ba410?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    },
+    {
+        title: "Montain Bike Track",
+        location: "The Bamboo Forest",
+        locUrl: "https://www.google.com/maps/place/Kyoto+Bike+Tour+-+The+Bamboo+Forest+%26+Arashiyama/@35.0194942,135.670815,18z/data=!3m1!4b1!4m5!3m4!1s0x6001a99346427e6f:0xd42663eba40842ac!8m2!3d35.0194942!4d135.6719093",
+        description:
+            "Kyoto's scenic nature and mountains are perfect for an unforgettable bike tour.",
+        month: 8,
+        img: "https://images.unsplash.com/photo-1575548393466-0df1618ba410?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    },
+    {
+        title: "Montain Bike Track",
+        location: "The Bamboo Forest",
+        locUrl: "https://www.google.com/maps/place/Kyoto+Bike+Tour+-+The+Bamboo+Forest+%26+Arashiyama/@35.0194942,135.670815,18z/data=!3m1!4b1!4m5!3m4!1s0x6001a99346427e6f:0xd42663eba40842ac!8m2!3d35.0194942!4d135.6719093",
+        description:
+            "Kyoto's scenic nature and mountains are perfect for an unforgettable bike tour.",
+        month: 9,
+        img: "https://images.unsplash.com/photo-1575548393466-0df1618ba410?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    },
+    {
+        title: "Night Matsuri",
+        location: "South Market",
+        locUrl: "https://www.google.com/maps/place/Kyoto+Bike+Tour+-+The+Bamboo+Forest+%26+Arashiyama/@35.0194942,135.670815,18z/data=!3m1!4b1!4m5!3m4!1s0x6001a99346427e6f:0xd42663eba40842ac!8m2!3d35.0194942!4d135.6719093",
+        description:
+            "Visit all the stands and eat all the food in the festival.",
+        month: 10,
+        img: "https://images.unsplash.com/photo-1565618991703-cdddef2bad22?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8eXVrYXRhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    },
+    {
+        title: "Fall Firewors",
+        location: "Shinji Bay",
+        locUrl: "https://www.google.com/maps/place/Kyoto+Bike+Tour+-+The+Bamboo+Forest+%26+Arashiyama/@35.0194942,135.670815,18z/data=!3m1!4b1!4m5!3m4!1s0x6001a99346427e6f:0xd42663eba40842ac!8m2!3d35.0194942!4d135.6719093",
+        description: "Enjoy an splending firework show near the sea.",
+        month: 10,
+        img: "https://images.unsplash.com/photo-1531686264889-56fdcabd163f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+        title: "Montain Bike Track",
+        location: "The Bamboo Forest",
+        locUrl: "https://www.google.com/maps/place/Kyoto+Bike+Tour+-+The+Bamboo+Forest+%26+Arashiyama/@35.0194942,135.670815,18z/data=!3m1!4b1!4m5!3m4!1s0x6001a99346427e6f:0xd42663eba40842ac!8m2!3d35.0194942!4d135.6719093",
+        description:
+            "Kyoto's scenic nature and mountains are perfect for an unforgettable bike tour.",
+        month: 10,
+        img: "https://images.unsplash.com/photo-1575548393466-0df1618ba410?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    },
+    {
+        title: "Winter Showcase",
+        location: "The Bamboo Forest",
+        locUrl: "https://www.google.com/maps/place/Kyoto+Bike+Tour+-+The+Bamboo+Forest+%26+Arashiyama/@35.0194942,135.670815,18z/data=!3m1!4b1!4m5!3m4!1s0x6001a99346427e6f:0xd42663eba40842ac!8m2!3d35.0194942!4d135.6719093",
+        description:
+            "Kyoto's scenic nature and mountains are perfect for an unforgettable bike tour.",
+        month: 11,
+        img: "https://images.unsplash.com/photo-1575548393466-0df1618ba410?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    },
+    {
+        title: "Montain Bike Track",
+        location: "The Bamboo Forest",
+        locUrl: "https://www.google.com/maps/place/Kyoto+Bike+Tour+-+The+Bamboo+Forest+%26+Arashiyama/@35.0194942,135.670815,18z/data=!3m1!4b1!4m5!3m4!1s0x6001a99346427e6f:0xd42663eba40842ac!8m2!3d35.0194942!4d135.6719093",
+        description:
+            "Kyoto's scenic nature and mountains are perfect for an unforgettable bike tour.",
+        month: 11,
+        img: "https://images.unsplash.com/photo-1575548393466-0df1618ba410?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    },
+    {
+        title: "Fireworks Late Year",
+        location: "The Bamboo Forest",
+        locUrl: "https://www.google.com/maps/place/Kyoto+Bike+Tour+-+The+Bamboo+Forest+%26+Arashiyama/@35.0194942,135.670815,18z/data=!3m1!4b1!4m5!3m4!1s0x6001a99346427e6f:0xd42663eba40842ac!8m2!3d35.0194942!4d135.6719093",
+        description:
+            "Kyoto's scenic nature and mountains are perfect for an unforgettable bike tour.",
+        month: 12,
+        img: "https://images.unsplash.com/photo-1575548393466-0df1618ba410?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    },
+    {
+        title: "Montain Bike Track",
+        location: "The Bamboo Forest",
+        locUrl: "https://www.google.com/maps/place/Kyoto+Bike+Tour+-+The+Bamboo+Forest+%26+Arashiyama/@35.0194942,135.670815,18z/data=!3m1!4b1!4m5!3m4!1s0x6001a99346427e6f:0xd42663eba40842ac!8m2!3d35.0194942!4d135.6719093",
+        description:
+            "Kyoto's scenic nature and mountains are perfect for an unforgettable bike tour.",
+        month: 12,
+        img: "https://images.unsplash.com/photo-1575548393466-0df1618ba410?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    },
 ];

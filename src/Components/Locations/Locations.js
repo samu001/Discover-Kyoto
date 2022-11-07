b06079e2dffd9717317f1f874eeb87049245f592
Samuel PF <samss01one@gmail.com>
2022-11-07 12:06:23 -0500

    testing api

--- src/Components/Locations/Locations.js
+++ src/Components/Locations/Locations.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import MapIcon from "./MapIcon";
 import "./Locations.css";
 import mapData from "../../Data/LocationsData";
+import axios from "axios";
 
 export default function Locations() {
     const [mapItems, setMapItems] = useState(mapData);
@@ -20,8 +21,33 @@ export default function Locations() {
         );
     });
 
+    const [articles, setArticles] = useState([""]);
+    let query = "pandemic";
+    let formattedArticles = [];
+
+    async function callAPI() {
+        let API_KEY = "pub_11407f5b05c5da9f5d7faa259db19175b2439";
+        const url = `https://newsdata.io/api/1/news?apikey=${API_KEY}&q=${query}&language=en`;
+
+        const res = await axios.get(url);
+        const articlesResponse = res.data.results;
+        for (let i = 0; i < Math.min(articlesResponse.length, 5); i++) {
+            const article = articlesResponse[i];
+            formattedArticles.push(article.title);
+        }
+
+        setArticles(formattedArticles);
+        console.log(articles);
+    }
+
     return (
         <>
+            <div>
+                <h5>Query for pandemic, returns 5 article titles</h5>
+                <button onClick={callAPI}>Make Call</button>
+                {articles}
+            </div>
+
             <div className="locations-section" id="locations">
                 <div className="map">
                     <div className="map-img-wrapper">
